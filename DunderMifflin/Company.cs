2009a280ecc8548894c1ee15d48ef7401f48a9a4
EvanG <evangdesigns@gmail.com>
2020-03-18 20:51:54 -0500

    Add project files.

---
+++ DunderMifflin/Company.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DunderMifflin
+{
+    public class Company
+    {
+        // Some properties
+        public string Name { get; }
+        public DateTime CreatedOn { get; }
+        public string Employee { get; set; }
+
+        //private List<Employee>
+    public Company(string name, DateTime createdOn)
+        {
+            Name = name;
+            CreatedOn = createdOn;
+        }
+
+    // Create a method that allows a caller to add an employee
+
+    // Create a method that allows a caller to remove an employee
+
+    // Create a method that allows a caller to retrieve the list of employees
+
+    }
+}
---
+++ DunderMifflin/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DunderMifflin
+{
+    class Employee
+    {
+        public string Name { get; }
+        public DateTime HiredOn { get; }
+
+        public Employee(string name, DateTime hiredOn)
+        {
+            Name = name;
+            HiredOn = hiredOn;
+        }
+    }
+}
